2d11f8532869f59133cb88e8bf361506144e9ed8
P <phernandeznyc@gmail.com>
2021-02-10 07:28:17 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,12 @@
 board = (" "," "," "," "," "," "," "," "," ")
+display_board(board)
 " "|" "|" "
 -----------
 " "|" "|" "
 -----------
 " "|" "|" "
 
-def display_board(board)
+def display_board
  puts " "|" "|" "
  puts -----------
  puts " "|" "|" "
